a72f2586192ed9f07b1e1874541ca10d5f101e98
Pranav Manoj <pranav.manoj@cal-saig.com>
2024-01-24 19:26:28 +0530

    Initial Changes

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -11,6 +11,7 @@ import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
 import Contoso from "../../assets/Contoso.svg";
+import karklogo from "../../assets/karklogo.svg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
@@ -588,7 +589,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={Contoso}
+                                    src={karklogo}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
